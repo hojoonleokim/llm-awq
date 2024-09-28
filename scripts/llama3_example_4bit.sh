991614f6abce9242c1975736afd8e009cb1ef5dd
HOJOONKIM <k296070@snu.ac.kr>
2024-09-28 19:04:31 +0900

    Update llama3_example_4bit.sh

--- scripts/llama3_example_4bit.sh
+++ scripts/llama3_example_4bit.sh
@@ -1,19 +1,19 @@
 MODEL=Meta-Llama-3-8B
 
 # run AWQ search (optional; we provided the pre-computed results)
-python CUDA_VISIBLE_DEVICES=1 -m awq.entry --model_path ../model/$MODEL \
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/Meta-Llama-3-8B \
     --w_bit 4 --q_group_size 128 \
     --run_awq --dump_awq awq_cache/$MODEL-w4-g128.pt
 
 # evaluate the AWQ quantize model (simulated pseudo quantization)
-python CUDA_VISIBLE_DEVICES=1 -m awq.entry --model_path ../model/$MODEL \
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
     --tasks wikitext \
     --w_bit 4 --q_group_size 128 \
     --load_awq awq_cache/$MODEL-w4-g128.pt \
     --q_backend fake
 
 # generate real quantized weights (w3)
-python CUDA_VISIBLE_DEVICES=1 -m awq.entry --model_path ../model/$MODEL \
+pCUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
     --w_bit 4 --q_group_size 128 \
     --load_awq awq_cache/$MODEL-w4-g128.pt \
     --q_backend real --dump_quant quant_cache/$MODEL-w4-g128-awq.pt
