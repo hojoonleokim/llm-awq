1ce8b5c2ae8d3ac407c3e8a55916e4bf7d674dac
HOJOONKIM <k296070@snu.ac.kr>
2024-09-30 19:33:34 +0900

    Update llama3-I-example_before_fakequant.sh

--- scripts/llama3-I-example_before_fakequant.sh
+++ scripts/llama3-I-example_before_fakequant.sh
@@ -1,10 +1,10 @@
 MODEL=Meta-Llama-3-8B-Instruct
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m awq.entry --model_path ../model/$MODEL \
+python -m awq.entry --model_path ../model/$MODEL \
     --w_bit 4 --q_group_size 128 \
     --run_awq --dump_awq awq_cache/$MODEL-w4-g128.pt
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m awq.entry --model_path ../model/$MODEL \
+python -m awq.entry --model_path ../model/$MODEL \
     --tasks wikitext \
     --w_bit 4 --q_group_size 128 \
     --load_awq awq_cache/$MODEL-w4-g128.pt \
