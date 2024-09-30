7aeeaa6054bcb95e189f21d6eb5a7d7b54542a28
HOJOONKIM <k296070@snu.ac.kr>
2024-09-30 15:29:05 +0900

    Update phi3-example.sh

--- scripts/phi3-example.sh
+++ scripts/phi3-example.sh
@@ -1,19 +1,19 @@
-MODEL=microsoft/Phi-3-medium-4k-instruct
+MODEL= Phi-3-medium-4k-instruct/
 
 # run AWQ search (optional; we provided the pre-computed results)
-CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path $MODEL \
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
     --w_bit 4 --q_group_size 128 \
     --run_awq --dump_awq awq_cache/$MODEL-w4-g128.pt
 
 # evaluate the AWQ quantize model (simulated pseudo quantization)
-CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path $MODEL \
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
     --tasks wikitext \
     --w_bit 4 --q_group_size 128 \
     --load_awq awq_cache/$MODEL-w4-g128.pt \
     --q_backend fake
 
 # generate real quantized weights (w4)
-CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path $MODEL \
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
     --w_bit 4 --q_group_size 128 \
     --load_awq awq_cache/$MODEL-w4-g128.pt \
     --q_backend real --dump_quant quant_cache/$MODEL-w4-g128-awq.pt
