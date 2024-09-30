dea1003890ae05cf7a0020053f4e82bc674cf04a
HOJOONKIM <k296070@snu.ac.kr>
2024-09-30 19:31:05 +0900

    edit script

--- scripts/llama3-I-example_before_fakequant.sh
+++ scripts/llama3-I-example_before_fakequant.sh
@@ -1,6 +1,6 @@
 MODEL=Meta-Llama-3-8B-Instruct
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m awq.entry --model_path /dataset/models/llama3/$MODEL \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m awq.entry --model_path ../model/$MODEL \
     --w_bit 4 --q_group_size 128 \
     --run_awq --dump_awq awq_cache/$MODEL-w4-g128.pt
 
