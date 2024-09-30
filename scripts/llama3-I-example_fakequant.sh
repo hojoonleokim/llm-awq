2dde8e13cfeb896199c8ca59bab478c990bbf4c8
HOJOONKIM <k296070@snu.ac.kr>
2024-09-30 19:41:37 +0900

    Update llama3-I-example_fakequant.sh

--- scripts/llama3-I-example_fakequant.sh
+++ scripts/llama3-I-example_fakequant.sh
@@ -1,11 +1,11 @@
 MODEL=Meta-Llama-3-8B-Instruct
 
-python -m awq.entry --model_path ../model/$MODEL \
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
     --w_bit 4 --q_group_size 128 \
     --run_awq --dump_awq awq_cache/$MODEL-w4-g128.pt
 
 # evaluate the AWQ quantize model (simulated pseudo quantization)
-python -m awq.entry --model_path ../model/$MODEL \
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
     --tasks wikitext \
     --w_bit 4 --q_group_size 128 \
     --load_awq awq_cache/$MODEL-w4-g128.pt \
