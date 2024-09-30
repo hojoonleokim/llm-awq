1c77fc6f33be1f6e2d80a03dec7f0a3d0d591e40
HOJOONKIM <k296070@snu.ac.kr>
2024-10-01 00:32:24 +0900

    edit

---
+++ scripts/phi_example_4bit-lutgemm.sh
@@ -0,0 +1,7 @@
+MODEL=Phi-3-medium-4k-instruct
+
+# generate real quantized weights (w3)
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
+    --w_bit 4 --q_group_size 128 \
+    --load_awq awq_cache/$MODEL-w4-g128.pt \
+    --q_backend real --dump_quant quant_cache/$MODEL-w4-g128-awq-lutgemm.pt
