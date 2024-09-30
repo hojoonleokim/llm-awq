01a66735dfa745a5d81cb357dc680bffc3bf0bb3
HOJOONKIM <k296070@snu.ac.kr>
2024-09-30 20:59:45 +0900

    bfq

--- scripts/llama3-I-example_before_fakequant.sh
+++ scripts/llama3-I-example_before_fakequant.sh
@@ -1,10 +1,6 @@
 MODEL=Meta-Llama-3-8B-Instruct
 
-python -m awq.entry --model_path ../model/$MODEL \
-    --w_bit 4 --q_group_size 128 \
-    --run_awq --dump_awq awq_cache/$MODEL-w4-g128.pt
-
-python -m awq.entry --model_path ../model/$MODEL \
+CUDA_VISIBLE_DEVICES=1 python -m awq.entry --model_path ../model/$MODEL \
     --tasks wikitext \
     --w_bit 4 --q_group_size 128 \
     --load_awq awq_cache/$MODEL-w4-g128.pt \
