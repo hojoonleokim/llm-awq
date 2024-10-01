1bfe1a30fd6b3907869d41cbca485abf24240720
HOJOONKIM <k296070@snu.ac.kr>
2024-10-01 16:02:55 +0900

    Update entry.py

--- awq/entry.py
+++ awq/entry.py
@@ -210,9 +210,10 @@ def build_model_and_enc(model_path):
                     os.makedirs(dirpath, exist_ok=True)
 
                     print(f"Saving the quantized model at {args.dump_quant}...")
+
                     filtered_state_dict = {k: v for k, v in model.cpu().state_dict().items() if not any(substr in k for substr in ['qweight', 'scaled_zeros', 'scales'])}
 
-                    torch.save(filtered_state_dict,args.output_name)
+                    torch.save(filtered_state_dict,args.output_path)
                     exit(0)
             else:
                 raise NotImplementedError
