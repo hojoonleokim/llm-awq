77c99c43f7216c1472cbdb46743b9d81a40a0664
HOJOONKIM <k296070@snu.ac.kr>
2024-10-08 01:52:36 +0900

    Update qmodule.py

--- awq/quantize/qmodule.py
+++ awq/quantize/qmodule.py
@@ -100,8 +100,8 @@ def convert_bcq_format(scale, zero, quant_data, qbits, do_packing=False, in_ch_w
         for n in range(N):
             for k in range(0, K, 32):
                 # torch.int32로 변환
-                binary_chunk = binary[k:k+32, b, n].to(torch.int32)
-                bit_values = torch.tensor([1 << i for i in range(32)], dtype=torch.int32, device='cuda')
+                binary_chunk = binary[k:k+32, b, n].to(torch.int64)
+                bit_values = torch.tensor([1 << i for i in range(32)], dtype=torch.int64, device='cuda')
                 
                 # 원소 곱셈 후 합산을 통해 내적 연산을 수행
                 s = torch.sum(binary_chunk * bit_values)
