56c610bb6da845f77d2561d5bb294195a007734c
HOJOONKIM <k296070@snu.ac.kr>
2024-10-08 01:11:38 +0900

    Update qmodule.py

--- awq/quantize/qmodule.py
+++ awq/quantize/qmodule.py
@@ -81,18 +81,19 @@ def convert_bcq_format(scale, zero, quant_data, qbits, do_packing=False, in_ch_w
     K = binary.shape[1] #input
     N = binary.shape[0] #output
 
-    scale = scale.permute(1,2,0).to(torch.device('cuda:0')).contiguous() # G B O
-    binary = binary.to(torch.device('cuda:0')) # I B O
-    offset = offset.permute(1,0).to(torch.device('cuda:0')).contiguous() # G O
+    scale = scale.permute(1,2,0).contiguous() # G B O
+    binary = binary # I B O
+    offset = offset.permute(1,0).contiguous() # G O
 
     bW = torch.zeros([K // 32, qbits, N], dtype=torch.int64).to(torch.device('cuda:0'))
 
     if do_packing == True:
-        for n in range(N):
-            for b in range(qbits):
-                for k in range(0, K, 32):
-                    s = 0
-                    for t in range(32):
+        #pragma omp parallel for
+        for b in range(qbits):
+            for k in range(0, K, 32):
+                s = 0
+                for t in range(32):
+                    for n in range(N):
                         if binary[n][k+t][b] == 1:
                             s |= (1 << t)  # 비트를 설정
                     bW[k // 32][b][n] = s
