9c0d65b7820ae2ba4fc9c1bd0125ebb5cc5ab854
HOJOONKIM <k296070@snu.ac.kr>
2024-10-24 21:25:24 +0900

    commit

--- scripts/llama3_example_4bit-lutgemm.sh
+++ scripts/llama3_example_4bit-lutgemm.sh
@@ -5,7 +5,7 @@ MODEL=Meta-Llama-3-8B-Instruct
 python -m awq.entry --model_path ../models/$MODEL \
     --w_bit 4 --q_group_size 128 \
     --load_awq awq_cache/$MODEL-w4-g128.pt \
-    --q_backend real --dump_quant quant_cache/$MODEL-w4-g128-lutgemm.pt
+    --q_backend real --dump_quant quant_cache/$MODEL-w3.5-g128-lutgemm.pt
 
 MODEL=Phi-3-medium-4k-instruct
 python -m awq.entry --model_path ../models/$MODEL \
