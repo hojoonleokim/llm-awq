2dddb2372dd1bef9f08c863913a7f6c76d9d9dd8
HOJOONKIM <k296070@snu.ac.kr>
2024-10-28 21:29:55 +0900

    Update entry.py

--- awq/entry.py
+++ awq/entry.py
@@ -252,7 +252,7 @@ def main():
     if args.tasks is not None:
         # https://github.com/IST-DASLab/gptq/blob/2d65066eeb06a5c9ff5184d8cebdf33662c67faf/llama.py#L206
         if args.tasks == "wikitext":
-            testenc = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+            testenc = load_dataset("mit-han-lab/pile-val-backup", split="test")
             testenc = enc("\n\n".join(testenc["text"]), return_tensors="pt")
             model.seqlen = 2048
             testenc = testenc.input_ids.to(model.device)
