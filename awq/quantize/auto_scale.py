13303ffffcf2d9586f7d75193b83d6abb1dad5d6
HOJOONKIM <k296070@snu.ac.kr>
2024-11-07 17:12:30 +0900

    Update auto_scale.py

--- awq/quantize/auto_scale.py
+++ awq/quantize/auto_scale.py
@@ -6,7 +6,7 @@ from transformers.models.bloom.modeling_bloom import BloomBlock, BloomGelu
 from transformers.models.opt.modeling_opt import OPTDecoderLayer
 from transformers.models.llama.modeling_llama import LlamaDecoderLayer, LlamaRMSNorm
 from transformers.activations import GELUActivation
-#from transformers.models.phi3.modeling_phi3 import Phi3RMSNorm
+from transformers.models.phi3.modeling_phi3 import Phi3RMSNorm
 
 from .qmodule import ScaledActivation
 from ..utils.module import get_op_by_name, get_op_name, set_op_by_name
