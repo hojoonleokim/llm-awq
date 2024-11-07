e18a5dc0980c1d47d18dc801dd0dcceb5b479fd5
HOJOONKIM <k296070@snu.ac.kr>
2024-11-07 15:02:30 +0900

    fake quant added

--- awq/entry.py
+++ awq/entry.py
@@ -198,7 +198,7 @@ def build_model_and_enc(model_path):
                 assert (
                     args.dump_quant is None
                 ), "Need to use real quantization to dump quantized weights"
-                pseudo_quantize_model_weight(model, w_bit=args.w_bit, q_config=q_config)
+                pseudo_quantize_model_weight(model, bit_pt=args.w_bit, q_config=q_config)
                 if args.dump_fake:
                     model.save_pretrained(args.dump_fake)
                     print("Pseudo-quantized models saved at", args.dump_fake)
